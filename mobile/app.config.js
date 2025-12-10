e77b5aed2f6fbdc94a67cde88ecf69250b0658dc
bluerat1 <delez.chamberlain@gmail.com>
2025-12-10 23:32:18 +0800

    minor changes

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -31,6 +31,17 @@ export default ({ config }) => {
     eas: {
       projectId: process.env.EXPO_PUBLIC_EAS_PROJECT_ID || '9ee38d41-83ad-4306-bff2-97c396db3856'
     },
+
+    firebaseApiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY || '',
+    firebaseAuthDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN || '',
+    firebaseProjectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID || '',
+    firebaseStorageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET || '',
+    firebaseMessagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || '',
+    firebaseAppId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID || '',
+    firebaseMeasurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID || '',
+
+
+
     cloudinaryCloudName: process.env.EXPO_PUBLIC_CLOUDINARY_CLOUD_NAME || '',
     cloudinaryUploadPreset: process.env.EXPO_PUBLIC_CLOUDINARY_UPLOAD_PRESET || '',
     cloudinaryApiKey: process.env.EXPO_PUBLIC_CLOUDINARY_API_KEY || '',
